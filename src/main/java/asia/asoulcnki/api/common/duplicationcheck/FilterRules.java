679b80af4565851b44a44eb793de6da81f9422ac
Tsubasa <lixuesonlu@qq.com>
2021-08-07 22:38:51 +0800

    fix:response with cache

--- src/main/java/asia/asoulcnki/api/common/duplicationcheck/FilterRules.java
+++ src/main/java/asia/asoulcnki/api/common/duplicationcheck/FilterRules.java
@@ -25,18 +25,39 @@ public class FilterRules {
 	}
 
 	public static Predicate<Reply> userIDIn(List<Integer> userIDs) {
-		return reply -> {
-			// default condition
-			if (userIDs == null || userIDs.isEmpty()) {
-				return true;
-			}
+		Predicate<Reply> userPredicate = new Predicate<Reply>() {
+            @Override
+            public boolean test(Reply reply) {
+                if (userIDs == null) {
+                    return true;
+                }
 
-			for (int id : userIDs) {
-				if (reply.getUid() == id) {
-					return true;
-				}
-			}
-			return false;
-		};
+                for (int id : userIDs) {
+                    if (reply.getMid() == id) {
+                        return true;
+                    }
+                }
+                return false;
+            }
+
+            @Override
+            public int hashCode() {
+                int hash = 0;
+                if (userIDs == null) {
+                    return hash;
+                }
+
+                for (int id : userIDs) {
+                    hash += id;
+                }
+                return hash;
+            }
+
+            @Override
+            public boolean equals(Object o) {
+                return this.hashCode() == o.hashCode();
+            }
+        };
+        return userPredicate;
 	}
 }
