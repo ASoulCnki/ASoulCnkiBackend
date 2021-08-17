071fa2ce7d8b33f029b2e02578c1738975af68fb
michaelfyc <michael.fyc@hdu.edu.cn>
2021-08-17 22:23:03 +0800

    chore: Add env for special test cases

--- src/test/java/asia/asoulcnki/api/StartTrainTest.java
+++ src/test/java/asia/asoulcnki/api/StartTrainTest.java
@@ -18,13 +18,16 @@ public class StartTrainTest extends AbstractTestNGSpringContextTests {
 
     @Test
     public void testStartTrain() {
-        File folder = new File("data/bilibili_cnki_reply.json");
-        assertTrue(folder.exists(), "需要在项目根目录下创建 data 文件夹，然后放入 bilibili_cnki_reply.json 文件捏");
-        File dataset = new File("data/database.dat");
-        if (!dataset.exists()) {
-            dataService.train();
+        // 非构建环境下需要训练结果文件
+        if (!"true".equals(System.getenv("GHA_BUILD_ENV"))) {
+            File folder = new File("data/bilibili_cnki_reply.json");
+            assertTrue(folder.exists(), "需要在项目根目录下创建 data 文件夹，然后放入 bilibili_cnki_reply.json 文件捏");
+            File dataset = new File("data/database.dat");
+            if (!dataset.exists()) {
+                dataService.train();
+            }
+            assertTrue(dataset.exists(), "未检测到训练结果文件，可能是训练结果文件名改变，请检查代码捏");
+            System.out.println("训练成功捏，训练结果已生成，接下来可以正常调取接口了捏");
         }
-        assertTrue(dataset.exists(), "未检测到训练结果文件，可能是训练结果文件名改变，请检查代码捏");
-        System.out.println("训练成功捏，训练结果已生成，接下来可以正常调取接口了捏");
     }
 }
