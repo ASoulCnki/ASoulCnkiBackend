92ec847227356dc03f447fff37b2bfe098a5d722
michaelfyc <michael.fyc@hdu.edu.cn>
2021-08-22 23:26:07 +0800

    fix: 更新 bin/sql 中 cnki 表结构并导入测试数据

--- bin/sql/cnki.sql
+++ bin/sql/cnki.sql
@@ -1,37 +1,35 @@
-create
-database if not exists bilibili_cnki;
+create database if not exists bilibili_cnki;
 
 use bilibili_cnki;
 
 drop table if exists user_dynamic;
-create table user_dynamic
-(
-    dynamic_id bigint comment '动态唯一id',
-    type_id    int comment '动态类型, 用于获取评论',
-    oid        bigint comment '动态对象id，用于获取评论',
-    status     int comment '评论爬取状态，0: 未爬取， 1: 至少爬取完成过一次',
-    primary key (dynamic_id)
+create table user_dynamic (
+    `dynamic_id` bigint comment '动态唯一 id',
+    `uid`        int comment '用户 id',
+    `type_id`    int comment '动态类型, 用于获取评论',
+    `oid`        bigint comment '评论区 id',
+    `status`     int comment '评论爬取状态，0: 未爬取， 1: 至少爬取完成过一次',
+    PRIMARY KEY (dynamic_id) USING BTREE
 ) DEFAULT CHARACTER SET = utf8mb4;
 
 drop table if exists kv;
-create table kv_store
-(
-    field_name  varchar(255) NOT NULL,
-    field_value text         NOT NULL,
-    primary key (field_name)
+create table kv_store (
+    `field_name`  varchar(255) NOT NULL,
+    `field_value` text         NOT NULL,
+    PRIMARY KEY (field_name) USING BTREE
 ) DEFAULT CHARACTER SET = utf8mb4;
 
 drop table if exists reply;
-create table reply
-(
-    rpid       bigint comment '回复id',
-    type_id    int,
-    dynamic_id bigint,
-    mid        int comment '成员id',
-    oid        bigint comment '源稿件id',
-    ctime      int comment '创建时间',
-    m_name     varchar(255) comment '昵称',
-    content    text comment '评论内容',
-    like_num   int,
-    primary key (rpid)
+create table reply (
+    `rpid`       bigint comment '回复id',
+    `uid`        int,
+    `type_id`    int,
+    `dynamic_id` bigint,
+    `mid`        int comment '成员id',
+    `oid`        bigint comment '源稿件id',
+    `ctime`      int comment '创建时间',
+    `m_name`     varchar(255) comment '昵称',
+    `content`    text comment '评论内容',
+    `like_num`   int,
+    PRIMARY KEY (rpid) USING BTREE
 ) DEFAULT CHARACTER SET = utf8mb4;
