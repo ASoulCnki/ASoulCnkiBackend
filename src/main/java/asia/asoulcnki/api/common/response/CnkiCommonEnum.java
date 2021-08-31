bf93b65f7eed16c27c412290565b73e8e32408e9
michael <michael.fyc@outlook.com>
2021-08-31 16:05:06 +0800

    refactor: 给部分代码添加注释或修改注释格式和内容

--- src/main/java/asia/asoulcnki/api/common/response/CnkiCommonEnum.java
+++ src/main/java/asia/asoulcnki/api/common/response/CnkiCommonEnum.java
@@ -1,41 +1,43 @@
 package asia.asoulcnki.api.common.response;
 
 public enum CnkiCommonEnum implements BaseErrorInfoInterface {
-	// 数据操作错误定义
-	SUCCESS(0, "success"), INVALID_REQUEST(400, "bad request"), BODY_NOT_MATCH(400, "invalid request format"),
-	SIGNATURE_NOT_MATCH(401, "signature not match"), NOT_FOUND(404, "resource not found"), INTERNAL_SERVER_ERROR(500,
-			"internal server error!"), SERVER_BUSY(503, "server is under strong traffic, please retry later"),
-	NO_TOKEN(401, "No jwt token presents"), AUTH(401, "JWT AUTH ERROR"), AUTH_USER_NOT_FOUND(401, "JWT AUTH ERROR"),
+    // 数据操作错误定义
+    SUCCESS(0, "success"),
+    INVALID_REQUEST(400, "bad request"),
+    BODY_NOT_MATCH(400, "invalid request format"),
+    SIGNATURE_NOT_MATCH(401, "signature not match"),
+    NOT_FOUND(404, "resource not found"),
+    INTERNAL_SERVER_ERROR(500, "internal server error!"),
+    SERVER_BUSY(503, "server is under strong traffic, please retry later"),
+    NO_TOKEN(401, "No JWT token presents"),
+    AUTH(401, "JWT auth error"),
+    AUTH_USER_NOT_FOUND(401, "JWT auth error"),
+    TEXT_TO_CHECK_TOO_LONG(20001, "text to check too long"),
+    TEXT_TO_CHECK_TOO_SHORT(20002, "text to check too short");
 
-	TEXT_TO_CHECK_TOO_LONG(20001, "text to check too long"),
-	TEXT_TO_CHECK_TOO_SHORT(20002, "text to check too short"),
+    /**
+     * 错误码
+     */
+    private final int resultCode;
 
-	// END
-	;
+    /**
+     * 错误描述
+     */
+    private final String resultMsg;
 
-	/**
-	 * 错误码
-	 */
-	private int resultCode;
+    CnkiCommonEnum(int resultCode, String resultMsg) {
+        this.resultCode = resultCode;
+        this.resultMsg = resultMsg;
+    }
 
-	/**
-	 * 错误描述
-	 */
-	private String resultMsg;
+    @Override
+    public int getResultCode() {
+        return resultCode;
+    }
 
-	CnkiCommonEnum(int resultCode, String resultMsg) {
-		this.resultCode = resultCode;
-		this.resultMsg = resultMsg;
-	}
-
-	@Override
-	public int getResultCode() {
-		return resultCode;
-	}
-
-	@Override
-	public String getResultMsg() {
-		return resultMsg;
-	}
+    @Override
+    public String getResultMsg() {
+        return resultMsg;
+    }
 
 }
--- src/main/java/asia/asoulcnki/api/service/IRankingService.java
+++ src/main/java/asia/asoulcnki/api/service/IRankingService.java
@@ -4,16 +4,39 @@ import asia.asoulcnki.api.common.duplicationcheck.FilterRulesContainer;
 import asia.asoulcnki.api.persistence.vo.RankingResultVo;
 
 public interface IRankingService {
-	RankingResultVo queryRankings(SortMethodEnum sortMethod, final TimeRangeEnum timeRange,
-			final FilterRulesContainer container, final int pageSize, final int pageNum);
+    /**
+     * 按指定规则筛选排序展示小作文
+     *
+     * @param sortMethod 排序方法
+     * @param timeRange  筛选时间
+     * @param container  过滤容器
+     * @param pageSize   单页显示小作文数
+     * @param pageNum    页数
+     * @return
+     */
+    RankingResultVo queryRankings(SortMethodEnum sortMethod, final TimeRangeEnum timeRange,
+                                  final FilterRulesContainer container, final int pageSize, final int pageNum);
 
-	void refresh();
+    /**
+     * 刷新排行
+     */
+    void refresh();
 
-	enum SortMethodEnum {
-		DEFAULT, LIKE_NUM, SIMILAR_COUNT
-	}
+    enum SortMethodEnum {
+        // 默认排序
+        DEFAULT,
+        // 按照点赞数排序
+        LIKE_NUM,
+        // 按照引用次数排序
+        SIMILAR_COUNT
+    }
 
-	enum TimeRangeEnum {
-		ALL, ONE_WEEK, THREE_DAYS
-	}
+    enum TimeRangeEnum {
+        // 在所有时间内筛选
+        ALL,
+        // 筛选一周内
+        ONE_WEEK,
+        // 筛选三天内
+        THREE_DAYS
+    }
 }
